a9e672e55ab6df303a2a43cd780d27572ba5c420
diatsilva007 <diatsilva007@gmail.com>
2025-11-03 20:11:23 -0300

    Adiciona funcionalidade ao Modal para exibir detalhes do chamado e implementa toggle no Dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -26,6 +26,9 @@ export default function Dashboard(){
   const [lastDocs, setLastDocs] = useState()
   const [loadingMore, setLoadingMore] = useState(false);
 
+  const [showPostModal, setShowPostModal] = useState(false);
+  const [detail, setDetail] = useState()
+
 
   useEffect(() => {
     async function loadChamados(){
@@ -87,6 +90,12 @@ export default function Dashboard(){
     const querySnapshot = await getDocs(q);
     await updateState(querySnapshot);
 
+  }
+
+
+  function toggleModal(item){
+    setShowPostModal(!showPostModal)
+    setDetail(item)
   }
 
 
@@ -156,7 +165,7 @@ export default function Dashboard(){
                         </td>
                         <td data-label="Cadastrado">{item.createdFormat}</td>
                         <td data-label="#">
-                          <button className="action" style={{ backgroundColor: '#3583f6' }}>
+                          <button className="action" style={{ backgroundColor: '#3583f6' }} onClick={ () => toggleModal(item)}>
                             <FiSearch color='#FFF' size={17}/>
                           </button>
                           <Link to={`/new/${item.id}`} className="action" style={{ backgroundColor: '#f6a935' }}>
@@ -178,7 +187,12 @@ export default function Dashboard(){
 
       </div>
 
-      <Modal/>
+      {showPostModal && (
+        <Modal
+          conteudo={detail}
+          close={ () => setShowPostModal(!showPostModal) }
+        />
+      )}
     
     </div>
   )
