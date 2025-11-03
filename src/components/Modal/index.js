2bf467af1436c2bb253e3503935d79d979a0455f
diatsilva007 <diatsilva007@gmail.com>
2025-11-03 20:19:13 -0300

    Refatora o componente Modal para melhorar a legibilidade do código e a formatação

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,12 +1,11 @@
+import { FiX } from "react-icons/fi";
+import "./modal.css";
 
-import { FiX } from 'react-icons/fi'
-import './modal.css';
-
-export default function Modal({ conteudo, close }){
-  return(
+export default function Modal({ conteudo, close }) {
+  return (
     <div className="modal">
       <div className="container">
-        <button className="close" onClick={ close }>
+        <button className="close" onClick={close}>
           <FiX size={25} color="#FFF" />
           Voltar
         </button>
@@ -31,24 +30,28 @@ export default function Modal({ conteudo, close }){
 
           <div className="row">
             <span>
-              Status: 
-              <i className="status-badge" style={{ color: "#FFF", backgroundColor: conteudo.status === 'Aberto' ? '#5cb85c' : '#999' }}>
+              Status:
+              <i
+                className="status-badge"
+                style={{
+                  color: "#FFF",
+                  backgroundColor:
+                    conteudo.status === "Aberto" ? "#5cb85c" : "#999",
+                }}
+              >
                 {conteudo.status}
               </i>
             </span>
           </div>
 
-          {conteudo.complemento !== '' && (
-          <>
-            <h3>Complemento</h3>
-            <p>
-              {conteudo.complemento}
-            </p>
-          </>
+          {conteudo.complemento !== "" && (
+            <>
+              <h3>Complemento</h3>
+              <p>{conteudo.complemento}</p>
+            </>
           )}
-
         </main>
       </div>
     </div>
-  )
+  );
 }
